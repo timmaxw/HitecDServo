7ff8a4e5849a7dfbbf1116a453c12d95e8ea0ad8
Tim Maxwell <timmaxw@gmail.com>
2022-07-02 10:28:12 -0700

    Move settings into their own file

---
+++ HitecDProgrammer/Settings.h
@@ -0,0 +1,15 @@
+#ifndef Settings_h
+#define Settings_h
+
+void changeIdSetting();
+void changeDirectionSetting();
+void changeSpeedSetting();
+void changeDeadbandSetting();
+void changeSoftStartSetting();
+void changeFailSafeSetting();
+void changePowerLimitSetting();
+void changeOverloadProtectionSetting();
+void changeSmartSenseSetting();
+void changeSensitivityRatioSetting();
+
+#endif /* Settings_h */
